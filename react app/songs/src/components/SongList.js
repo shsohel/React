b0fbb53cbf2aafe6f0def614aaf4962cbd0560f9
SH SOHEL <49935124+shsohel@users.noreply.github.com>
2020-12-02 07:39:39 +0600

    sdda
    adad

---
+++ react app/songs/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class SongList extends React.Component {
+    render() {
+        return <div> SongList </div>;
+    }
+}
+const mapStateToProps = state => {
+    console.log(state)
+    return state;
+}
+
+export default connect(mapStateToProps)(SongList);
